192aff22520cee9662d213b60551db4d2c1c2ac9
Michiel Roos <michiel@michielroos.com>
2018-09-05 16:44:11 +0200

    Add Rules for TYPO3 7 Breaking-61823-RemoveMagicSettterForFromTC

---
+++ config/Matcher/v7/PropertyProtectedMatcher.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    // Properties changed from public to protected
+    'TYPO3\CMS\Core\Database\RelationHandler->fromTC' => [
+        'restFiles' => [
+            'Breaking-61823-RemoveMagicSettterForFromTC.rst',
+        ],
+    ],
+];
