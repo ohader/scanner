362ee64df9cd071c6c93162d2412b563945118d4
Michiel Roos <michiel@michielroos.com>
2018-09-05 15:03:26 +0200

    Add Matcher for TYPO3 7 Breaking-57382-FlashMessageApi

---
+++ config/Matcher/v7/ClassNameMatcher.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    // Removed classes
+    'TYPO3\CMS\Core\Messaging\FlashMessage' => [
+        'restFiles' => [
+            'Breaking-57382-FlashMessageApi.rst',
+        ],
+    ],
+];
