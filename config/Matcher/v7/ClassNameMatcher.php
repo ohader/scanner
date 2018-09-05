82a939c3d89250dae2e7d69388a56d3c0e7a2508
Michiel Roos <michiel@michielroos.com>
2018-09-05 16:16:27 +0200

    Add Rules for TYPO3 7 Breaking-61782-DeprecatedDocumentTemplateClassesRemoved

--- config/Matcher/v7/ClassNameMatcher.php
+++ config/Matcher/v7/ClassNameMatcher.php
@@ -6,4 +6,19 @@ return [
             'Breaking-57382-FlashMessageApi.rst',
         ],
     ],
+    'TYPO3\CMS\Backend\Template\MediumDocumentTemplate' => [
+        'restFiles' => [
+            'Breaking-61782-DeprecatedDocumentTemplateClassesRemoved.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\Template\SmallDocumentTemplate' => [
+        'restFiles' => [
+            'Breaking-61782-DeprecatedDocumentTemplateClassesRemoved.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\Template\StandardDocumentTemplate' => [
+        'restFiles' => [
+            'Breaking-61782-DeprecatedDocumentTemplateClassesRemoved.rst',
+        ],
+    ],
 ];
