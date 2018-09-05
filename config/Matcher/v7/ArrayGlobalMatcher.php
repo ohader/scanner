38ec5b50eea09c60b60b9f003aef95f6bb5a63f9
Michiel Roos <michiel@michielroos.com>
2018-09-05 15:42:36 +0200

    Add Rule for TYPO3 7 Breaking-60559-DroppedBackendLoginOptions

--- config/Matcher/v7/ArrayGlobalMatcher.php
+++ config/Matcher/v7/ArrayGlobalMatcher.php
@@ -5,4 +5,14 @@ return [
             'Breaking-53542-DeprecatedCodeRemovalInFluid.rst',
         ],
     ],
+    '$GLOBALS[\'TBE_STYLES\'][\'loginBoxImage_rotationFolder\']' => [
+        'restFiles' => [
+            'Breaking-60559-DroppedBackendLoginOptions.rst',
+        ],
+    ],
+    '$GLOBALS[\'TBE_STYLES\'][\'loginBoxImage_author\']' => [
+        'restFiles' => [
+            'Breaking-60559-DroppedBackendLoginOptions.rst',
+        ],
+    ],
 ];
