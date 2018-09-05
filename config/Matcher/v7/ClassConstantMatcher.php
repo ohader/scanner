dd3f72f0f11192330de4e179272d816a834f94b5
Michiel Roos <michiel@michielroos.com>
2018-09-05 16:46:00 +0200

    Add Rules for TYPO3 7 Breaking-61859-FileTypeSoftwareRemoved

---
+++ config/Matcher/v7/ClassConstantMatcher.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'TYPO3\CMS\Core\Resource\AbstractFile::FILETYPE_SOFTWARE' => [
+        'restFiles' => [
+            'Breaking-61859-FileTypeSoftwareRemoved.rst',
+        ],
+    ],
+];
