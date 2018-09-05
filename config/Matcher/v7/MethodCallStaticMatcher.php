4ada5262a8d8747861b3c25a94fb282b3a4bf129
Michiel Roos <michiel@michielroos.com>
2018-09-05 15:31:14 +0200

    Add Rules for TYPO3 7 Breaking-59659-DeprecatedCodeRemovalInBackendSysext

---
+++ config/Matcher/v7/MethodCallStaticMatcher.php
@@ -0,0 +1,60 @@
+<?php
+return [
+    // Removed methods
+    'TYPO3\CMS\Backend\RecordList\AbstractRecordList::writeBottom' => [
+        'numberOfMandatoryArguments' => 0,
+        'maximumNumberOfArguments' => 0,
+        'restFiles' => [
+            'Breaking-59659-DeprecatedCodeRemovalInBackendSysext.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\Sprite\SpriteGenerator::setOmmitSpriteNameInIconName' => [
+        'numberOfMandatoryArguments' => 1,
+        'maximumNumberOfArguments' => 1,
+        'restFiles' => [
+            'Breaking-59659-DeprecatedCodeRemovalInBackendSysext.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\Template\DocumentTemplate::isCMlayers' => [
+        'numberOfMandatoryArguments' => 0,
+        'maximumNumberOfArguments' => 0,
+        'restFiles' => [
+            'Breaking-59659-DeprecatedCodeRemovalInBackendSysext.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\Template\DocumentTemplate::getFileheader' => [
+        'numberOfMandatoryArguments' => 3,
+        'maximumNumberOfArguments' => 3,
+        'restFiles' => [
+            'Breaking-59659-DeprecatedCodeRemovalInBackendSysext.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\Utility\BackendUtility::displayWarningMessages' => [
+        'numberOfMandatoryArguments' => 0,
+        'maximumNumberOfArguments' => 0,
+        'restFiles' => [
+            'Breaking-59659-DeprecatedCodeRemovalInBackendSysext.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\Utility\IconUtility::getIconImage' => [
+        'numberOfMandatoryArguments' => 3,
+        'maximumNumberOfArguments' => 5,
+        'restFiles' => [
+            'Breaking-59659-DeprecatedCodeRemovalInBackendSysext.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\View\PageLayoutView::getSelectedBackendLayoutUid' => [
+        'numberOfMandatoryArguments' => 1,
+        'maximumNumberOfArguments' => 1,
+        'restFiles' => [
+            'Breaking-59659-DeprecatedCodeRemovalInBackendSysext.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\ClickMenu\ClickMenu::menuItemsForClickMenu' => [
+        'numberOfMandatoryArguments' => 1,
+        'maximumNumberOfArguments' => 1,
+        'restFiles' => [
+            'Breaking-59659-DeprecatedCodeRemovalInBackendSysext.rst',
+        ],
+    ],
+];
