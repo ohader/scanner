e10974a9ca2ae0f63a3901f316d9bb10c35e82be
Michiel Roos <michiel@michielroos.com>
2018-09-05 17:08:59 +0200

    Add Rules for TYPO3 7 Breaking-62038-RemovedDocumentTemplateOptions

---
+++ config/Matcher/v7/PropertyPublicMatcher.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    // Removed public properties
+    'TYPO3\CMS\Backend\Template\DocumentTemplate->JScodeLibArray' => [
+        'restFiles' => [
+            'Breaking-62038-RemovedDocumentTemplateOptions.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\Template\DocumentTemplate->docType' => [
+        'restFiles' => [
+            'Breaking-62038-RemovedDocumentTemplateOptions.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\Template\DocumentTemplate->inDocStyles' => [
+        'restFiles' => [
+            'Breaking-62038-RemovedDocumentTemplateOptions.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\Template\DocumentTemplate->inDocStyles_TBEstyle' => [
+        'restFiles' => [
+            'Breaking-62038-RemovedDocumentTemplateOptions.rst',
+        ],
+    ],
+    'TYPO3\CMS\Backend\Template\DocumentTemplate->charset' => [
+        'restFiles' => [
+            'Breaking-62038-RemovedDocumentTemplateOptions.rst',
+        ],
+    ],
+];
