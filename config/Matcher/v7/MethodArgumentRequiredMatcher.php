23b4b51bdaacff456e4b6d78be68b235c406691d
Michiel Roos <michiel@michielroos.com>
2018-09-06 23:01:22 +0200

    Add Rules for TYPO3 7 Important-67248-CleanUpDataMapperconvertClassNameToTableName

---
+++ config/Matcher/v7/MethodArgumentRequiredMatcher.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'TYPO3\CMS\Extbase\Persistence\Generic\Mapper\DataMapper->convertClassNameToTableName' => [
+        'numberOfMandatoryArguments' => 1,
+        'maximumNumberOfArguments' => 1,
+        'restFiles' => [
+            'Important-67248-CleanUpDataMapperconvertClassNameToTableName.rst',
+        ],
+    ],
+];
