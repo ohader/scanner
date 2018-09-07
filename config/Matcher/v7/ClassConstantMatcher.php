60c451df061feb7bc1b2ca2baa4e1e221a33c353
Michiel Roos <michiel@michielroos.com>
2018-09-07 12:24:31 +0200

    Add Rules for TYPO3 7 Breaking-69227-StringsForLikeAreNotProperlyEscaped

--- config/Matcher/v7/ClassConstantMatcher.php
+++ config/Matcher/v7/ClassConstantMatcher.php
@@ -5,4 +5,24 @@ return [
             'Breaking-61859-FileTypeSoftwareRemoved.rst',
         ],
     ],
+    'TYPO3\CMS\IndexedSearch\Controller\SearchFormController::WILDCARD_LEFT' => [
+        'restFiles' => [
+            'Breaking-69227-StringsForLikeAreNotProperlyEscaped.rst',
+        ],
+    ],
+    'TYPO3\CMS\IndexedSearch\Controller\SearchFormController::WILDCARD_RIGHT' => [
+        'restFiles' => [
+            'Breaking-69227-StringsForLikeAreNotProperlyEscaped.rst',
+        ],
+    ],
+    'TYPO3\CMS\IndexedSearch\Domain\Repository\IndexSearchRepository::WILDCARD_LEFT' => [
+        'restFiles' => [
+            'Breaking-69227-StringsForLikeAreNotProperlyEscaped.rst',
+        ],
+    ],
+    'TYPO3\CMS\IndexedSearch\Domain\Repository\IndexSearchRepository::WILDCARD_RIGHT' => [
+        'restFiles' => [
+            'Breaking-69227-StringsForLikeAreNotProperlyEscaped.rst',
+        ],
+    ],
 ];
