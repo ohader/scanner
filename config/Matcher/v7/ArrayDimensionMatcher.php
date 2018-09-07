8f389b4145104a81a71f0cf31272419368576b18
Michiel Roos <michiel@michielroos.com>
2018-09-07 08:27:18 +0200

    Move rules from ArrayGlobalMatcher to ArrayDimensionMatcher

---
+++ config/Matcher/v7/ArrayDimensionMatcher.php
@@ -0,0 +1,130 @@
+<?php
+return [
+    // LocalConfiguration / AdditionalConfiguration settings
+    '$GLOBALS[\'TBE_STYLES\'][\'loginBoxImage_rotationFolder\']' => [
+        'restFiles' => [
+            'Breaking-60559-DroppedBackendLoginOptions.rst',
+        ],
+    ],
+    '$GLOBALS[\'TBE_STYLES\'][\'loginBoxImage_author\']' => [
+        'restFiles' => [
+            'Breaking-60559-DroppedBackendLoginOptions.rst',
+        ],
+    ],
+    '$GLOBALS[\'TBE_MODULES_EXT\'][\'xMOD_alt_clickmenu\'][\'extendCMclasses\'][\'path\']' => [
+        'restFiles' => [
+            'Breaking-61781-IncludeOnceArrayOfClickMenuControllerRemoved.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'SC_OPTIONS\'][\'t3lib/utility/class.t3lib_utility_mail.php\'][\'substituteMailDelivery\']' => [
+        'restFiles' => [
+            'Breaking-61783-RemoveDeprecatedMailFunctionality.rst',
+        ],
+    ],
+    '$GLOBALS[\'TBE_STYLES\'][\'colorschemes\']' => [
+        'restFiles' => [
+            'Breaking-61890-Remove-TBE-Styling-From-FormEngine.rst',
+        ],
+    ],
+    '$GLOBALS[\'TBE_STYLES\'][\'styleschemes\']' => [
+        'restFiles' => [
+            'Breaking-61890-Remove-TBE-Styling-From-FormEngine.rst',
+        ],
+    ],
+    '$GLOBALS[\'TBE_STYLES\'][\'borderschemes\']' => [
+        'restFiles' => [
+            'Breaking-61890-Remove-TBE-Styling-From-FormEngine.rst',
+        ],
+    ],
+    '$GLOBALS[\'TBE_STYLES\'][\'mainColors\']' => [
+        'restFiles' => [
+            'Breaking-62039-RemovedTBE_StylesMainColors.rst',
+        ],
+    ],
+    '$GLOBALS[\'TBE_STYLES\'][\'background\']' => [
+        'restFiles' => [
+            'Deprecation-62988-DocumentTemplateFunctions.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'SYS\'][\'compat_version\']' => [
+        'restFiles' => [
+            'Breaking-24900-CompatVersion-Setting-Removed.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'BE\'][\'accessListRenderMode\']' => [
+        'restFiles' => [
+            'Breaking-64226-OptionAccessListRenderModeRemoved.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'GFX\'][\'enable_typo3temp_db_tracking\']' => [
+        'restFiles' => [
+            'Breaking-64643-RemoveEnableTypo3tempDbTracking.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'FE\'][\'secureFormmail\']' => [
+        'restFiles' => [
+            'Breaking-64668-MailformMovedToLegacyExtension.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'FE\'][\'strictFormmail\']' => [
+        'restFiles' => [
+            'Breaking-64668-MailformMovedToLegacyExtension.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'FE\'][\'formmailMaxAttachmentSize\']' => [
+        'restFiles' => [
+            'Breaking-64668-MailformMovedToLegacyExtension.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'SC_OPTIONS\'][\'GLOBAL\'][\'softRefParser_GL\']' => [
+        'restFiles' => [
+            'Deprecation-64109-Hook-softRefParserGL.rst',
+        ],
+    ],
+    '$GLOBALS[\'MCONF\'][\'_\']' => [
+        'restFiles' => [
+            'Breaking-65432-ModuleUriInGlobalVarRemoved.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'FE\'][\'activateContentAdapter\']' => [
+        'restFiles' => [
+            'Breaking-66034-DropContentAdapter.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'BE\'][\'loginNewsTitle\']' => [
+        'restFiles' => [
+            'Breaking-66431-NewLoginScreen.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'SYS\'][\'lockingMode\']' => [
+        'restFiles' => [
+            'Deprecation-47712-DeprecateOldLockingAPI.rst',
+        ],
+    ],
+    '$GLOBALS[\'TBE_STYLES\'][\'logo_login\']' => [
+        'restFiles' => [
+            'Deprecation-66431-NewLoginScreen.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'SC_OPTIONS\'][\'typo3/index.php\'][\'loginScriptHook\']' => [
+        'restFiles' => [
+            'Breaking-66669-BackendLoginControllerRefactored.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'SC_OPTIONS\'][\'typo3/index.php\'][\'loginFormHook\']' => [
+        'restFiles' => [
+            'Breaking-66669-BackendLoginControllerRefactored.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'GFX\'][\'png_to_gif\']' => [
+        'restFiles' => [
+            'Breaking-66906-AutomaticPNGToGIFConversionRemoved.rst',
+            'Deprecation-66906-FunctionalityPngToGifConversion.rst',
+        ],
+    ],
+    '$GLOBALS[\'TYPO3_CONF_VARS\'][\'DB\'][\'extTablesDefinitionScript\']' => [
+        'restFiles' => [
+            'Deprecation-65344-ExtTables.rst',
+        ],
+    ],
+];
